cde51f55cf36eb6f4efa316de8b4d069e01fd864
Felipe Chama <felipe@moises.ai>
2023-10-22 23:50:39 -0300

    site

--- site/app/(default)/page.tsx
+++ site/app/(default)/page.tsx
@@ -1,8 +1,8 @@
 export const metadata = {
   title: "Oliveira & Melli - Advogados Associados",
-  description: "",
+  description: "Soluções jurídicas completas",
+  image: "/images/logo.jpg",
 };
-
 import Hero from "@/components/hero";
 import Features from "@/components/features";
 import FeaturesBlocks from "@/components/features-blocks";
